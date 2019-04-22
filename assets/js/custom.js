0f619975261a3d5a62a58d1d103863083d0aaba3
adi_solomon <andrian.solomon@highcontrast.ro>
2019-04-22 18:40:25 +0300

    Menu design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,13 @@ function get_random(){
     var rand = pos[Math.floor(Math.random() * pos.length)];
     return rand;
 }
+
+jQuery(document).ready(function($) {
+    $('.open-menu').on('click', function(){
+        $('.site-menu').toggleClass('show-menu');
+        $('.nav-menu').toggleClass('hide-nav');
+        $('body').toggleClass('hide-scroll');
+
+        console.log('Show');
+    });
+});
